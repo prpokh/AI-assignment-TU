92cbbfb5d4751ee9cddb8fe8b7d9def24af9aab1
Prasanna Pokharel <prasannapokharel12621@gmail.com>
2026-09-04 10:59:50 +0545

    added DLS algorithm

---
+++ Lab 1(Python)/DLS.py
@@ -0,0 +1,24 @@
+from common_data import graph, start_node, goal_node, show_student_info
+
+def dls(graph, node, goal, limit, path):
+    path.append(node)
+    if node == goal:
+        return True
+    if limit <= 0:
+        path.pop()
+        return False
+        
+    for neighbor, _ in graph.get(node, []):
+        if neighbor not in path:
+            if dls(graph, neighbor, goal, limit - 1, path):
+                return True
+                
+    path.pop()
+    return False
+
+limit = 4
+path = []
+found = dls(graph, start_node, goal_node, limit, path)
+
+print(f"Goal {goal_node} reached within limit {limit}: {path if found else 'Not Found'}")
+show_student_info()
